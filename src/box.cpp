d85735bb4b2db21cb0d48abb9e2eae3333c24e57
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-11-27 19:42:38 +0530

    Add box struct

---
+++ src/box.cpp
@@ -0,0 +1,7 @@
+namespace Game {
+    struct Box {
+        float GRAVITY = 9.8f;
+        int width;
+        int height;
+    };
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <print>
 #include <vec3.hpp>
+#include <thread>
+#include <chrono>
+
+int TPS = 60;
 
 int main() {
-    std::println("nparticles");
-    return 0;
+    std::print("\033[2J\033[H");
+    std::println("nparticles 0.1.0");
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::print("\033[2J\033[H");
+    std::this_thread::sleep_for(std::chrono::seconds(2));   
 }
