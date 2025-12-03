4a339a40b483d1ee6b7cfec63b52e31cab134837
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-12-03 16:09:20 +0530

    fix: lock to 24fps and add wall declaration

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,39 @@
-#include <print>
-#include <vec3.hpp>
 #include <thread>
 #include <chrono>
 #include <random>
+#include <print>
+#include <vec3.hpp>
 #include "./engine.hpp"
 
-const int TPS = 60;
+const int TPS = 24;
 const float DESIRED_DT = 1.0f / TPS;
 std::chrono::duration<float> DESIRED_DT_CHRONO{DESIRED_DT};
 std::chrono::milliseconds DESIRED_DT_CHRONO_MS = std::chrono::duration_cast<std::chrono::milliseconds>(DESIRED_DT_CHRONO);
-const int N = 100000;
+const int N = 10;
 const int NVEC = 5;
-// const int WALL_HEIGHT = 100;
-// const int WALL_WIDTH = 50;
 const float GFORCE = -9.8f;
 
+const int WALL_HEIGHT = 100;
+const int WALL_WIDTH = 50;
+
+void printWall(int width, int height) {
+    //std::println("-");
+    for (int i = 0; i < width; i++) std::printf("-");
+}
 
 int main() {
+
     typedef std::chrono::high_resolution_clock Time;
     typedef std::chrono::milliseconds ms;
     typedef std::chrono::duration<float> fsec;
     typedef std::chrono::seconds sec;
+
     std::println("\033[2J\033[Hnparticles 0.1.0");
+    printWall(WALL_WIDTH, WALL_HEIGHT);
+
     std::random_device rd; // Obtain a random number from hardware
     std::mt19937 gen(rd()); // Seed the Mersenne Twister engine with a random device
-    std::uniform_real_distribution<float> dis(0.0f, 1.0f); // Define a uniform distribution for floats
+    std::uniform_real_distribution<float> dis(-1.0f, 1.0f); // Define a uniform distribution for floats
     std::this_thread::sleep_for(sec(1));
     std::print("\033[2J\033[H");
 
@@ -34,7 +43,7 @@ int main() {
     }
     auto prevfinish = Time::now();
 
-    long long tick = 0;
+    long long frames = 0;
     // sim loop
     while (true) {
         // current time, chrono duration
@@ -49,23 +58,24 @@ int main() {
             fps = (int) (1000.0f / dt.count());
         }
         std::print("\033[2J\033[H");
-        std::println("{}\tTick: {}\n{}FPS\n{} particles\n", dt, tick, fps, N);
+        std::println("{}\tFrame: {}\n{}FPS\n{} particles\n", dt, frames, fps, N);
+        printWall(WALL_WIDTH, WALL_HEIGHT);
         for (int i = 0; i < N; i++) {
             float fx = dis(gen);
             float fy = dis(gen);
             float fz = dis(gen);
             p[i].doTick(deltat.count(), nvec3::Vec3(fx, fy, fz));
         }
-        for (int i = 0; i < NVEC; i++) std::println("Particle {} : {}", i, p[i].pos);
-	tick++;
+        // DEBUG: print vec vals
+        // for (int i = 0; i < NVEC; i++) std::println("Particle {} : {}", i, p[i].pos);
+	    frames++;
         auto work_done = Time::now();
         ms work_dt = std::chrono::duration_cast<ms>(work_done - curriter);
         ms tts = DESIRED_DT_CHRONO_MS - work_dt;
 
         prevfinish = curriter;
-	// uncomment to lock to a certain fps
-        //if (tts.count() > 0) {
-        //    std::this_thread::sleep_for(tts);
-        //}
+        if (tts.count() > 0) {
+            std::this_thread::sleep_for(tts);
+        }
     }
 }
